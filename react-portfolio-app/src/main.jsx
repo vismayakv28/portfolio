32a4dc1153748e4384217b178cb2aa53c382c201
vismaya-kv28 <vismayakv258@gmail.com>
2025-12-16 13:51:57 +0530

    Fix Vercel 404

--- react-portfolio-app/src/main.jsx
+++ react-portfolio-app/src/main.jsx
@@ -1,8 +1,7 @@
+import { BrowserRouter } from "react-router-dom";
 
-
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
